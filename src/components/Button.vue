e829636a415025b26f5a16525b7798b9c945a99a
fadhlimulyana20 <baba.fadhli@gmail.com>
2021-06-01 10:59:11 +0700

    Refactor onClick

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,6 +1,5 @@
 <template>
   <button
-    @click="callback()"
     class="rounded bg-green-500 text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-opacity-50 px-2 py-1"
   >
     {{ text }}
@@ -14,11 +13,6 @@ export default {
     text: {
       type: String,
       default: "Button",
-    },
-  },
-  methods: {
-    callback() {
-      this.$emit("click");
     },
   },
 };
